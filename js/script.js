8971448d900aaad4b553937036a5cb2991283f87
SUK TARA <suktara447@gmail.com>
2024-10-13 16:16:50 +0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,31 @@
+let marks = 105
+
+if(marks <1 || marks >100){
+    console.log("paknami kom karo prio");
+
+}
+
+
+else if(marks >=33 && marks <50) {
+    console.log("you got c");
+    
+
+}
+else if(marks >=50  && marks <60){
+    console.log("you got B");
+
+}
+else if(marks >=60  && marks <70){
+    console.log("you got A-");
+} 
+else if(marks >=70  && marks <80){
+    console.log("you got A");
+}
+else if(marks >=80  && marks <100){
+    console.log("you got A+");
+}  
+
+
+else{
+    console.log("you are fail");
+}
